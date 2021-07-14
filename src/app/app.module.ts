d4880841d1921d9545adec4faf786a5362286358
Nikos Batzonis <n.batzonis@gmail.com>
2021-07-14 13:42:05 +0200

    Add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'Cat',
+    price: 800,
+    description: 'bla bla bla',
+  },
+  {
+    name: 'Dog',
+    price: 1200,
+    description: 'bla bla bla',
+  },
+  {
+    name: 'Parrot',
+    price: 300,
+    description: 'bla bla bla',
+  },
+];
